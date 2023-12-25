7495e64bb3b0cde7edbdd5eba28b8c064583084c
Reiyo OISHI <reiyo.icepp@gmail.com>
2023-12-25 21:09:09 +0900

    Cosmetics

--- backend/src/PageGenerator.py
+++ backend/src/PageGenerator.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pprint
+import json
 
 # Logging
 import logging
@@ -15,78 +16,134 @@ logger.addHandler(stream_handler)
 # Handmade modules
 sys.path.append(os.path.dirname(os.pardir))
 from OpenAI.src.TextGeneration import TextGeneration
-from OpenAI.utils.checkOpenAIAPIKeyValid import CheckAPIKeyValid as CheckOpenAIAPIKeyValid
-from OpenAI.utils.payloadParsor import PayloadParsor
 from OpenAI.utils.textCosmetics import TextCosmetics
 from OpenAI.utils.textEmbedding import TextEmbedding, CosSimilarities
 from OpenAI.utils.textFromCSV import ParseSubtitleCSV
 from DeepLAPI.src.translator import DeepLTranslator
-from DeepLAPI.utils.checkDeepLAPIKeyValid import CheckAPIKeyValid as CheckDeepLAPIKeyValid
 
-def GetSimilarTextsAndStartFrames(texts: list[str], startframes: list[int], subject: str, nreturn: int=1):
-  emb_texts = TextEmbedding(texts=texts)
-  emb_subject = TextEmbedding(texts=subject)
-  cosines = CosSimilarities(vec1s=emb_texts, vec2=emb_subject[-1])
+class PageGenerator(object):
+  def __init__(
+    self,
+    title: str='「座りっぱなし」は寿命が縮む',
+    csvfile: str='../assets/helth.csv',
+    n_summary_texts: int=5,
+    max_tokens_per_call: int=500,
+  ) -> None:
+    self.llm = TextGeneration(max_tokens_per_call=max_tokens_per_call)
+    self.translator = DeepLTranslator()
 
-  information = [[text, startframe, cosine] for text, startframe, cosine in zip(texts, startframes, cosines)]
-  information = sorted(information, key=lambda x: x[2], reverse=True)
-  return information[:nreturn]
+    self.title = title
+    self.csvfile = csvfile
+    self.subtitle = ParseSubtitleCSV(csvfile=csvfile)
+    self.n_summary_texts = n_summary_texts
 
-llm = TextGeneration()
-translator = DeepLTranslator()
+  def set_title(self, title: str) -> None:
+    self.title = title
 
-CheckDeepLAPIKeyValid(api_key=os.environ['DEEPL_API_KEY'])
-CheckOpenAIAPIKeyValid(api_key=os.environ['OPENAI_API_KEY'])
+  def set_csvfile(self, csvfile: str) -> None:
+    self.csvfile = csvfile
+    self.subtitle = ParseSubtitleCSV(csvfile=csvfile)
 
-# sys.exit(1)
+  def set_n_summary_texts(self, n_summary_texts: int) -> None:
+    self.n_summary_texts = n_summary_texts
 
-llm.add_message_entry_as_specified_role_with_text_content(
-  role='system',
-  text=translator.translate(
-    text='あなたは質問に対してカジュアルかつ簡潔に回答するアシスタントです。',
-    source_lang='JA',
-    target_lang='EN-US',
-  )
-)
+  def check_openai_api_key_valid(self, api_key: str) -> None:
+    self.llm.set_api_key(api_key=api_key)
 
-csvfile = '../assets/helth.csv'
-title = '「座りっぱなし」は寿命が縮む'
-subtitle = ParseSubtitleCSV(csvfile=csvfile)
-llm.add_message_entry_as_specified_role_with_text_content(
-  role='user',
-  text=translator.translate(
-    text=TextCosmetics(
-      text=f'''\
-        以下に続く文章は"{title}"というタイトルのニュース番組を文字起こししたものです。\
-        動画の内容を要約してください。\
-        "{''.join(subtitle['texts'])}"\
-      '''
-    ),
-    source_lang='JA',
-    target_lang='EN-US',
-  )
-)
+  def check_deepl_api_key_valid(self, api_key: str) -> None:
+    self.translator.set_api_key(api_key=api_key)
 
-payload = llm.get_payload()
-for line in pprint.pformat(PayloadParsor(payload=payload), width=150).split('\n'):
-  logger.info(line)
+  def get_similar_text_startframes(self, texts: list[str], startframes: list[int], subject: str, nreturns: int=1) -> list[str]:
+    emb_texts = TextEmbedding(texts=texts)
+    emb_subject = TextEmbedding(texts=subject)
+    cosines = CosSimilarities(vec1s=emb_texts, vec2=emb_subject[-1])
 
-llmresponse = llm.execute()
-llmresponse = llmresponse.choices[0].message.content.replace('\n', '')
-llmresponse = translator.translate(
-  text=llmresponse,
-  source_lang='EN',
-  target_lang='JA',
-)
+    information = [info for info in zip(texts, startframes, cosines)]
+    information = sorted(information, key=lambda x: x[2], reverse=True)
 
-for llmres in llmresponse.split('。'):
-  if len(llmres) > 0:
-    goodsubtitles = GetSimilarTextsAndStartFrames(
-      texts=subtitle['texts'],
-      startframes=subtitle['startframes'],
-      subject=llmres,
-      nreturn=1,
+    return information[:nreturns]
+
+  def set_system_character(self, text: str) -> None:
+    self.llm.add_message_entry_as_specified_role_with_text_content(
+      role='system',
+      text=self.translator.translate(
+        text=text, source_lang='JA', target_lang='EN-US',
+      )
     )
 
-    print(f'*  {llmres}')
-    print(f'-> {goodsubtitles[0]}')
+  def set_subtitle_texts(self) -> None:
+    self.llm.add_message_entry_as_specified_role_with_text_content(
+      role='user',
+      text=self.translator.translate(
+        text=TextCosmetics(
+          text=f'''
+            以下に続く文章は"{self.title}"というタイトルのニュース番組を文字起こししたものです。\
+            動画の内容を{self.n_summary_texts}つの文章に要約し、文章の配列として出力してください。\
+            "{''.join(self.subtitle['texts'])}"
+          '''
+        ),
+        source_lang='JA',
+        target_lang='EN-US',
+      ),
+    )
+
+  def set_function_tool(self) -> None:
+    def dummy_function(responses: list[str]):
+      for response in responses:
+        logger.info(response)
+
+    self.llm.add_tool_entry_as_function(
+      tools={
+        'type': 'function',
+        'function': {
+          'name': dummy_function.__name__,
+          'description': 'Dummy function for controling the GPT output format.',
+          'parameters': {
+            'type': 'object',
+            'properties': {
+              'responses': {'type': 'array', 'items': {'type': 'string'}},
+            },
+            'required': ['responses'],
+          },
+        },
+      },
+    )
+
+  def print_llm_payload(self):
+    payload = self.llm.get_payload()
+    for line in pprint.pformat(payload, width=150).split('\n'):
+      logger.info(line)
+
+  def execute(self):
+    llmresponse = self.llm.execute()
+    llmresponse = json.loads(llmresponse.choices[0].message.tool_calls[0].function.arguments)['responses']
+    for llmres in llmresponse:
+      llmres = self.translator.translate(
+        text=llmres, source_lang='EN', target_lang='JA',
+      )
+
+      goodsubtitles = self.get_similar_text_startframes(
+        texts=self.subtitle['texts'],
+        startframes=self.subtitle['startframes'],
+        subject=llmres,
+        nreturns=1,
+      )
+
+      logger.info(f'*  {llmres}')
+      logger.info(f'-> {goodsubtitles[0]}')
+
+if __name__ == '__main__':
+  obj = PageGenerator()
+  obj.check_openai_api_key_valid(
+    api_key=os.environ['OPENAI_API_KEY']
+  )
+  obj.check_deepl_api_key_valid(
+    api_key=os.environ['DEEPL_API_KEY']
+  )
+  obj.set_system_character(
+    text='あなたは質問に対してカジュアルかつ簡潔に回答するアシスタントです。'
+  )
+  obj.set_subtitle_texts()
+  obj.set_function_tool()
+  obj.print_llm_payload()
+  obj.execute()
